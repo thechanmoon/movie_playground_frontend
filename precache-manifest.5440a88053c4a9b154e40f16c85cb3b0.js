2764b623c36d79b40f78d9de8b1fddd0329e7c39
chan <thechanmoon@gmail.com>
2020-07-22 10:44:11 -0400

    Updates

---
+++ precache-manifest.5440a88053c4a9b154e40f16c85cb3b0.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8ce7677c1f24acad8d8029a6b0cc00b7",
+    "url": "/movie_playground/index.html"
+  },
+  {
+    "revision": "d99f0563e722a4291e6b",
+    "url": "/movie_playground/static/css/main.f45bfcdc.chunk.css"
+  },
+  {
+    "revision": "5f9ea6da547830b8680c",
+    "url": "/movie_playground/static/js/2.a607c992.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/movie_playground/static/js/2.a607c992.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d99f0563e722a4291e6b",
+    "url": "/movie_playground/static/js/main.b12297b0.chunk.js"
+  },
+  {
+    "revision": "c52cd6f8bf5d6c4af39a",
+    "url": "/movie_playground/static/js/runtime-main.d12fd10a.js"
+  },
+  {
+    "revision": "f4404720ece11355df318a0acf525cb1",
+    "url": "/movie_playground/static/media/loading.f4404720.gif"
+  },
+  {
+    "revision": "0028a5984c3d2db160a365e5217995c8",
+    "url": "/movie_playground/static/media/logo_playground.0028a598.png"
+  }
+]);
