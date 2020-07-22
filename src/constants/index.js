84260d81b4404ae76a0d0be256313e08a3e74946
chan <thechanmoon@gmail.com>
2020-07-22 10:47:48 -0400

    homepage config changed

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,4 +1,4 @@
- export const API_URL = "http://localhost:3000"
-//export const API_URL = "https://moviepg-api.herokuapp.com/"
+//export const API_URL = "http://localhost:3000"
+export const API_URL = "https://moviepg-api.herokuapp.com/"
 // export const IMAGE_URL = "https://image.tmdb.org/t/p/w300"
 export const IMAGE_URL = "https://image.tmdb.org/t/p/w300_and_h450_bestv2"
